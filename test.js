59b32391e3da5f20d493c69d334e329274af0315
singhsapna3593 <111954795+singhsapna3593@users.noreply.github.com>
2022-08-24 11:45:30 +0530

    html

---
+++ test.js
@@ -0,0 +1,78 @@
+//  var s = 3;
+//  var x = 5;
+
+//  console.log(s+x);
+  // x=2;
+console.log(10**2);
+let x = 'mar'
+switch(x){
+case 'jan':
+case 'mar':
+case 'may':
+case 'july':
+case 'aug':
+case 'oct':
+case 'dec':
+
+console.log('31')
+break;
+case 'apr':
+case 'jun':
+case 'sep':
+case 'nov':
+  console.log('30') 
+  break;
+}
+let f = '5'
+var output = '';
+switch(f){
+case '1':
+  output = 'monday'
+case '2':
+  output = 'monday'
+ case '3':
+  case '4':
+    case '5':
+    case '6':
+    case '7':
+console.log()
+
+}
+
+function get(m) {
+  switch(m){
+    case 1: return 'monday'
+    case 2: return 'test'
+    case 3: return 'web'
+    case 4: return 'fr'
+
+  }
+}
+
+console.log(get(1));
+function xyz(a,b);
+console.log(a=1)
+{
+  // console.log(a<=30)
+}
+// xyz(a=a+2)
+function getnumber(number){
+switch(month){
+
+}
+
+
+
+
+}
+let even = "Even Number" + "<br>";
+let odd = "Odd Number" + "<br>";
+
+for (let i = 1; i <= 10; i++) {
+	if(i % 2 === 0){
+    	even += i  +" - "+ (i*2) + "<br/>";
+    } else {
+    	odd += i +" - " + (i*5) + "<br/>";
+    }
+}
+console.log(even + '<br />' + odd)
